efe406bf69d1437d4159db98ffb7b2f8e5852a54
Mateusz Janowski <mat.krz.janowski@gmail.com>
2021-10-26 12:07:20 +0200

    Added htmlfor in ReactHookComponent

--- src/containers/ReactHookForm/ReactHookForm.js
+++ src/containers/ReactHookForm/ReactHookForm.js
@@ -14,7 +14,6 @@ export default function App() {
 
 	const onSubmit = (data) => {
 		setFormSubmitted(true);
-		console.log(data);
 	};
 
 	const onConfirmHandle = () => {
@@ -27,14 +26,24 @@ export default function App() {
 	const reactForm = (
 		<form style={styles.form} onSubmit={handleSubmit(onSubmit)}>
 			<div style={styles.container}>
-				<label style={styles.label}>Name</label>
-				<input style={styles.input} type='text' {...register('name', { required: true, maxLength: 80 })} />
+				<label htmlFor='name' style={styles.label}>
+					Name
+				</label>
+				<input
+					id='name'
+					style={styles.input}
+					type='text'
+					{...register('name', { required: true, maxLength: 80 })}
+				/>
 				{errors.name && <span style={styles.requiredError}>This field is required</span>}
 			</div>
 
 			<div style={styles.container}>
-				<label style={styles.label}>Email</label>
+				<label htmlFor='email' style={styles.label}>
+					Email
+				</label>
 				<input
+					id='email'
 					style={styles.input}
 					type='email'
 					{...register('email', { required: true, pattern: /^\S+@\S+$/i })}
@@ -43,8 +52,10 @@ export default function App() {
 			</div>
 
 			<div style={styles.container}>
-				<label style={styles.label}>Bio</label>
-				<textarea style={styles.textarea} {...register('bio', { required: true })} />
+				<label htmlFor='bio' style={styles.label}>
+					Bio
+				</label>
+				<textarea id='bio' style={styles.textarea} {...register('bio', { required: true })} />
 				{errors.bio && <span style={styles.requiredError}>This field is required</span>}
 			</div>
 
@@ -52,29 +63,38 @@ export default function App() {
 				<span style={styles.label}>Sex</span>
 				<div>
 					<input
+						id='female'
 						style={styles.radioInput}
 						{...register('sex', { required: true })}
 						type='radio'
 						value='Female'
 					/>
-					<label>Female</label>
+					<label htmlFor='femal'>Female</label>
 				</div>
 				<div>
 					<input
+						id='male'
 						style={styles.radioInput}
 						{...register('sex', { required: true })}
 						type='radio'
 						value='Male'
 					/>
-					<label>Male</label>
+					<label htmlFor='Male'>Male</label>
 				</div>
 				{errors.sex && <span style={styles.requiredError}>This field is required</span>}
 			</div>
 
 			<div style={styles.container}>
 				<div>
-					<input style={styles.checkbox} type='checkbox' {...register('terms', { required: true })} />
-					<label style={styles.checkboxLabel}>I agree to the terms of service</label>
+					<input
+						id='terms'
+						style={styles.checkbox}
+						type='checkbox'
+						{...register('terms', { required: true })}
+					/>
+					<label htmlFor='terms' style={styles.checkboxLabel}>
+						I agree to the terms of service
+					</label>
 				</div>
 
 				{errors.terms && <span style={styles.requiredError}>This field is required</span>}
